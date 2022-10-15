42d054c6a8e4fa21ecb918816b714c286ff26146
Gabriel Rufino <contato@gabrielrufino.com>
2022-10-15 12:11:23 -0300

    Controller for the private route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import { PublicController } from './controllers/public.controller'
+import { PrivateController } from './controllers/private.controller'
 
 const {
   PORT = 3000
@@ -9,5 +10,6 @@ const {
 const app = express()
 
 app.get('/public', PublicController.get)
+app.get('/private', PrivateController.get)
 
 app.listen(PORT)
