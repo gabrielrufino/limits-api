28a9bf722d9b538a9a4bc5d9031a72b70e5ad036
Gabriel Rufino <contato@gabrielrufino.com>
2022-10-15 13:50:17 -0300

    Logger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import pino from 'pino-http'
 
 import { PublicController } from './controllers/public.controller'
 import { PrivateController } from './controllers/private.controller'
+import { logger } from './logger'
 
 const {
   PORT = 3000
@@ -9,7 +11,9 @@ const {
 
 const app = express()
 
+app.use(pino())
+
 app.get('/public', PublicController.get)
 app.get('/private', PrivateController.get)
 
-app.listen(PORT)
+app.listen(PORT, () => logger.info(`Server is running at ${PORT}`))
